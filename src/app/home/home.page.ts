f1d917807f595d3506178a6d836f18fb888885b6
koalter <heavens_drive090@hotmail.com>
2023-10-13 17:24:46 -0300

    correcciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
@@ -9,14 +9,59 @@ import { LanguageService } from '../services/language.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  readonly assets = {
+    languages: {
+      es: '../../assets/icon/es.png',
+      en: '../../assets/icon/en.png',
+      pt: '../../assets/icon/pt.png'
+    },
+    themes: {
+      animal: '../../assets/icon/animal.png',
+      color: '../../assets/icon/colour.png',
+      number: '../../assets/icon/number.png'
+    }
+  };
+
+  langSrc: string = this.assets.languages.es;
+  themeSrc: string = this.assets.themes.color;
 
   constructor(
     private auth: AuthService,
     private router: Router,
     private loadingController: LoadingController,
-    public language: LanguageService
+    private language: LanguageService
   ) {}
+
+  ngOnInit(): void {
+    const url = this.router.routerState.snapshot.url.split('/');
+    switch (this.language.get()) {
+      case LanguageService.EN:
+        this.langSrc = this.assets.languages.en;
+        break;
+      case LanguageService.PT:
+        this.langSrc = this.assets.languages.pt;
+        break;
+      case LanguageService.ES:
+      default:
+        this.langSrc = this.assets.languages.es;
+        break;
+    }
+    
+    switch (url[2]) {
+      case 'animals':
+        this.themeSrc = this.assets.themes.animal;
+        break;
+      case 'numbers':
+        this.themeSrc = this.assets.themes.number;
+        break;
+      case 'colors':
+      default:
+        this.themeSrc = this.assets.themes.color;
+        break;
+    }
+  }
 
   async logout() {
     const loadingElement = await this.loadingController
@@ -29,5 +74,32 @@ export class HomePage {
     if (result) {
       this.router.navigate(['login']);
     }
+  }
+
+  setToSpanish() {
+    this.language.setToSpanish();
+    this.langSrc = this.assets.languages.es;
+  }
+
+  setToEnglish() {
+    this.language.setToEnglish();
+    this.langSrc = this.assets.languages.en;
+  }
+
+  setToPortuguese() {
+    this.language.setToPortuguese();
+    this.langSrc = this.assets.languages.pt;
+  }
+  
+  playAnimals() {
+    this.themeSrc = this.assets.themes.animal;
+  }
+
+  playColors() {
+    this.themeSrc = this.assets.themes.color;
+  }
+
+  playNumbers() {
+    this.themeSrc = this.assets.themes.number;
   }
 }
--- src/assets/texts.ts
+++ src/assets/texts.ts
@@ -1,12 +1,12 @@
 const colors = new Map();
-colors.set("es-ES", ["rojo", "verde", "amarillo", "blanco", "negro"]);
-colors.set("en-US", ["red", "green", "yellow", "white", "black"]);
-colors.set("pt-PT", ["vermelho", "verde", "amarelo", "branco", "preto"]);
+colors.set("es-ES", ["rojo", "verde", "amarillo", "blanco", "negro", "celeste", "rosa", "gris", "marrón", "violeta"]);
+colors.set("en-US", ["red", "green", "yellow", "white", "black", "light blue", "pink", "grey", "brown", "violet"]);
+colors.set("pt-PT", ["vermelho", "verde", "amarelo", "branco", "preto", "céu azul", "rosa", "cinza", "marrom", "tolet"]);
 
 const animals = new Map();
-animals.set("es-ES", ["perro", "gato", "vaca", "pato", "caballo"]);
-animals.set("en-US", ["dog", "cat", "cow", "duck", "horse"]);
-animals.set("pt-PT", ["cachorro", "gato", "vaca", "pato", "cavalo"]);
+animals.set("es-ES", ["perro", "gato", "vaca", "pato", "caballo", "tortuga", "loro", "elefante", "jirafa", "león"]);
+animals.set("en-US", ["dog", "cat", "cow", "duck", "horse", "turtle", "parrot", "elephant", "giraffe", "lion"]);
+animals.set("pt-PT", ["cachorro", "gato", "vaca", "pato", "cavalo", "tartaruga", "papagaio", "girafa", "leão"]);
 
 const numbers = new Map();
 numbers.set("es-ES", ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"]);
